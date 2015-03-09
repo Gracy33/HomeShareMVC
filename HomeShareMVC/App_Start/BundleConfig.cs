354829140e713b770115c5eb172b6df4cdaaa428
Gracy33 <gracemingiedi@gmail.com>
2015-03-09 20:21:10 +0100

    BundleConfig+Layout

--- HomeShareMVC/App_Start/BundleConfig.cs
+++ HomeShareMVC/App_Start/BundleConfig.cs
@@ -10,21 +10,21 @@ namespace HomeShareMVC
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-1.9.1.min.js",
-                        "~/Scripts/bootstrap.js",
                         "~/Scripts/script.js",
                         "~/Scripts/modernizr.custom.79639.js",
                         "~/Scripts/jquery.ba-cond.min.js",
-                        "~/Scripts/jquery.slitslider.js"
+                        "~/Scripts/jquery.slitslider.js",
+                        "~/Scripts/owl.carousel.js"                        
                         ));
 
-            //// Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
-            //// prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
+            // prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
 
             bundles.Add(new StyleBundle("~/Content/css")
@@ -34,7 +34,9 @@ namespace HomeShareMVC
                "~/Content/css/style.css",
                "~/Content/css/Site.css",
                "~/Content/css/slitslider/style.css",
-               "~/Content/css/slitslider/custom.css"
+               "~/Content/css/slitslider/custom.css",
+               "~/Content/css/owl-carousel/owl.carousel.css",
+               "~/Content/css/owl-carousel/owl.theme.css"               
                ));
         }
     }
